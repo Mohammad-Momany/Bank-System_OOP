e49e5d1bdbcb1249f2ac0056fd44785381a5a438
Mohammad-Momany <malmomany@avertra.com>
2024-09-29 11:56:47 +0300

    Find Currency Screen

--- Bank System_OOP/clsCurrencyExchangeMainScreen.h
+++ Bank System_OOP/clsCurrencyExchangeMainScreen.h
@@ -4,6 +4,7 @@
 #include "clsInputValidate.h"
 #include <iomanip>
 #include "clsCurrenciesListScreen.h"
+#include "clsFindCurrencyScreen.h"
 
 using namespace std;
 
@@ -32,13 +33,14 @@ private:
 
     static void _ShowCurrenciesListScreen()
     {
+        //  cout << "\nCurriencies List Screen Will Be Here.\n";
         clsCurrenciesListScreen::ShowCurrenciesListScreen();
     }
 
     static void _ShowFindCurrencyScreen()
     {
-        cout << "\nFind Currency Screen Will Be Here.\n";
-
+        //cout << "\nFind Currency Screen Will Be Here.\n";
+        clsFindCurrencyScreen::ShowFindCurrencyScreen();
     }
 
     static void _ShowUpdateCurrencyRateScreen()
